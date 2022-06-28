72997f654b8eebd021469821d5e5f070a55877f9
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-06-28 12:09:06 -0500

    helper-files

--- controllers/files.controller.js
+++ controllers/files.controller.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const { request, response } = require("express");
 
 const cargarArchivo = async (req = request, res = response) => {
@@ -6,20 +5,6 @@ const cargarArchivo = async (req = request, res = response) => {
     const error = new Error("No hay archivos en la peticion");
     return res.status(400).json({ msg: error.message });
   }
-
-  const { archivo } = req.files;
-
-  const uploadPath = path.join(__dirname, "../uploads/", archivo.name);
-
-  archivo?.mv(uploadPath, (err) => {
-    if (err) {
-      console.log(err.message);
-      const error = new Error(`Algo salio mal ${err.message}`);
-      return res.status(500).json({ msg: error.message });
-    }
-
-    res.json({ msg: `El archivo se subio al path ${uploadPath}` });
-  });
 };
 
 module.exports = {
---
+++ helpers/subir-archivo.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const { v4: uuidev4 } = require("uuid");
+const subirArchivo = (
+  files,
+  extencionValidas = ["png", "jpg", "gif", "jpeg"],
+  carpeta = ""
+) => {
+  return new Promise((resolve, reject) => {
+    const { archivo } = files;
+    const nombreCortado = archivo.name.split(".");
+    const extencion = nombreCortado[nombreCortado.length - 1];
+
+    if (!extencionValidas.includes(extencion)) {
+      return reject(
+        `El tipo de archivo ${archivo.name} no contiene una extencion valida: ${extencionValidas}`
+      );
+    }
+    const nombreTemp = uuidev4() + "." + extencion;
+
+    const uploadPath = path.join(__dirname, "../uploads/", carpeta, nombreTemp);
+
+    archivo?.mv(uploadPath, (err) => {
+      if (err) {
+        console.log(err.message);
+        return reject(`Algo salio mal ${err.message}`);
+      }
+
+      resolve(uploadPath);
+    });
+  });
+};
+
+module.exports = subirArchivo;
