86cfdfce909f1826dcb45877829df2160d4cb872
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-01 18:04:21 -0500

    si

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,23 @@
-const socket = io();
+const usuario = null;
+const socket = null;
+const validarJWT = async () => {
+  const token = localStorage.getItem("token") || "";
+  if (token.length <= 10) {
+    window.location = "index.html";
+    throw new Error("no hay token en el servidlor");
+  }
+  const respuesta = await fetch("http://localhost:3001/api/auth", {
+    headers: { "x-token": token },
+  });
+
+  const { usuario: userDB, token: tokenDB } = await respuesta.json();
+  localStorage.setItem("token", tokenDB);
+  usuario: userDB;
+};
+const main = async () => {
+  await validarJWT();
+};
+
+main();
+
+// const socket = io();
